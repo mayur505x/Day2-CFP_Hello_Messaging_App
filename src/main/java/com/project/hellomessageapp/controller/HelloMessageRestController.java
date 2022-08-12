1ef9df0b1d46887ef9560feda6528f9a6e7b1554
Admin <mayurpawar8691@gmail.com>
2022-08-12 13:29:03 +0530

    Added: Display Message using Query Parameter

--- src/main/java/com/project/hellomessageapp/controller/HelloMessageRestController.java
+++ src/main/java/com/project/hellomessageapp/controller/HelloMessageRestController.java
@@ -2,6 +2,7 @@ package com.project.hellomessageapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class HelloMessageRestController {
     @GetMapping(value = {"", "/", "/home"})
     public String index() {
         return "Hello from Bridgelabz!!!!!";
+    }
+
+    @GetMapping(value = "/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from Bridgelabz";
     }
 }
